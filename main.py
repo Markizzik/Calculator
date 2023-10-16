c2c8fee6f958298cfe8ff85c49d669001a180ca9
Markizzik <146652957+Markizzik@users.noreply.github.com>
2023-10-16 16:21:43 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+app = FastAPI()
+@app.get("/sum")
+def sum(a: int, b: int):
+    """Сложение"""
+    return a + b
+
+@app.get("/difference")
+def difference(a: int, b: int):
+    """Вычитание"""
+    return a - b
+
+@app.get("/multiplication")
+def multiplication(a: int, b: int):
+    """Умножение"""
+    return a * b
+
+@app.get("/division")
+def division(a: int, b: int):
+    """ Деление"""
+    if b == 0:
+        return "Ахтунг!!! Делить на 0 нельзя!"
+    else:
+        return a / b
